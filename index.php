bf6b88436ec7bd70eeb76414591d7e643065c238
WnX <vandermeulen.christophe@gmail.com>
2022-03-14 12:47:54 +0100

    userliste

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-$pays = [
-    'Sélectionner',
-    'France',
-    'Belgique',
-    'Espagne',
-    'Portugal',
-    'Allemagne'
-];
+session_start();
+require_once('pays.php');
+
+
+$position = null;
+if(isset($_GET['position'])){
+    $position = $_GET['position'];
+    $user = $_SESSION['user'][$position];
+}
+
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,10 +22,19 @@ $pays = [
 </head>
 
 <body>
+    <?php if($position=== null) : ?>
+        <h1>Ajout</h1>
+    <?php else: ?>
+        <h1>Edition</h1>
+    <?php endif; ?>
     <form action="sauvegarde.php" method="POST">
-        Nom : <input type="text" name="nom"><br> Prenom : <input type="text" name="prenom"><br> Pseudo : <input type="text" name="pseudo"><br> Date de Naissance : <input type="date" name="datedenaissance"><br> Pays :
+        Nom : <input type="text" name="nom" value="<?= $user['nom']?>">
+        <br> Prenom : <input type="text" name="prenom" value="<?= $user['prenom']?>">
+        <br> Pseudo : <input type="text" name="pseudo" value="<?= $user['pseudo']?>">
+        <br> Date de Naissance : <input type="date" name="datedenaissance" value="<?= $user['datedenaissance']?>"><br> Pays :
         <select name="pays">
             <?php foreach ($pays as $p) :  ?>
+                <?php $checked = '';?>
                 <option value="<?= $p ?>"><?= $p ?></option>
             <?php endforeach; ?>
         </select><br> Mot de passe: <input type="password" name="password"><br>
--- liste.php
+++ liste.php
@@ -3,9 +3,11 @@ session_start();
 
 if (isset($_SESSION['user'])) {
     echo '<table>';
-    echo '<tr><td>Nom</td><td>Prenom</td></tr>';
-    foreach ($_SESSION['user'] as $utilisateur) {
-        echo '<tr><td>'. $utilisateur['nom'] . ' ' . $utilisateur['prenom'] . ' ' .$utilisateur['pseudo']. ' ' . $utilisateur['datedenaissance']. ' ' . $utilisateur['pays']. ' '. $utilisateur['password'].  '</Td></tr>';
+    echo '<tr><td>ID</td><td>Nom</td><td>Prenom</td><td>pseudo</td><td>Date de naissance</td><td>pays</td><td>password</td><td>modifier</td><td>Supprimer</td></tr>';
+    foreach ($_SESSION['user'] as $position => $utilisateur) {
+        echo "<tr><td>$position</td><td>" . $utilisateur['nom'] . '</td><td>' . $utilisateur['prenom'] . '</td><td>' . $utilisateur['pseudo'] . '</td><td>' . $utilisateur['datedenaissance'] . '</td><td>' . $utilisateur['pays'] . '</td><td>' . $utilisateur['password'] .  '</Td>';
+        echo '<td><a href="index.php?position=' . $position . '">Modifier</a></td>
+        <td><a href="supprimer.php?position=' . $position . '">Supprimer</a></td></tr>';
     }
     echo '</table>';
 }
