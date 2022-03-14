6a9d711adf222961fcb68b0181fa873ae09437cb
winnux82 <vandermeulen.christophe@gmail.com>
2022-03-14 15:26:05 +0100

    supprimer

--- supprimer.php
+++ supprimer.php
@@ -1,11 +1,13 @@
 <?php
+
+// on démarre la session
 session_start();
-
+// on définit la variable position
 $position = $_GET['position'];
-
+// si la position existe, 
 if (isset($position)) {
-
+//on efface la ligne position de la session user
     unset($_SESSION['user'][$position]);
-
+//on redirige vers la liste
     header('location: liste.php');
 }
