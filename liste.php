4744d2e603678925a151b305f1ce25e77f0c46ff
winnux82 <vandermeulen.christophe@gmail.com>
2022-03-14 13:37:44 +0100

    :alien: table

--- liste.php
+++ liste.php
@@ -5,7 +5,14 @@ if (isset($_SESSION['user'])) {
     echo '<table>';
     echo '<tr><td>ID</td><td>Nom</td><td>Prenom</td><td>pseudo</td><td>Date de naissance</td><td>pays</td><td>password</td><td>modifier</td><td>Supprimer</td></tr>';
     foreach ($_SESSION['user'] as $position => $utilisateur) {
-        echo "<tr><td>$position</td><td>" . $utilisateur['nom'] . '</td><td>' . $utilisateur['prenom'] . '</td><td>' . $utilisateur['pseudo'] . '</td><td>' . $utilisateur['datedenaissance'] . '</td><td>' . $utilisateur['pays'] . '</td><td>' . $utilisateur['password'] .  '</Td>';
+        echo "<tr><td>$position</td><td>" . $utilisateur['nom'] . '</td>';
+        echo '<td>' . $utilisateur['prenom'] . '</td>';
+        echo '<td>' . $utilisateur['pseudo'] . '</td>';
+        echo '<td>' . $utilisateur['datedenaissance'] . '</td>';
+        echo '<td>' . $utilisateur['pays'] . '</td>';
+        echo '<td>' . $utilisateur['password'] .  '</td>';
+
+        
         echo '<td><a href="index.php?position=' . $position . '">Modifier</a></td>
         <td><a href="supprimer.php?position=' . $position . '">Supprimer</a></td></tr>';
     }
